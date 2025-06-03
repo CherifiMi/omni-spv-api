8e7ee939ccfb0991b112b60a38651af7ca30f2be
cherifimi <cherifi154949@gmail.com>
2025-06-03 13:41:40 +0100

    first

---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"net/http"
+)
+
+var collection *mongo.Collection
+
+type Document map[string]interface{}
+
+func main() {
+	initMongoDB()
+
+	r := gin.Default()
+
+	r.GET("/", test)
+	r.POST("/spv", createDocument)
+	r.PUT("/spv/:id", updateDocument)
+	r.GET("/spv", getAllDocuments)
+	r.GET("/spv/:id", getDocumentByID)
+
+	r.Run(":8080")
+
+}
+
+func initMongoDB() {
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("mongodb+srv://cherifim:aEiogaT5aQXq6sev@welledge.a9nzvcy.mongodb.net/?retryWrites=true&w=majority&appName=welledge").SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	//defer func() {
+	//	if err = client.Disconnect(context.TODO()); err != nil {
+	//		panic(err)
+	//	}
+	//}()
+
+	collection = client.Database("omni").Collection("spv1")
+
+	// Send a ping to confirm a successful connection
+	if err := collection.Database().RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	fmt.Println("Collection assigned:", collection != nil)
+	fmt.Println("Using collection:", collection.Name())
+}
+
+func test(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Hello Mito!"})
+}
+
+func createDocument(c *gin.Context) {
+	var doc Document
+	if err := c.ShouldBindJSON(&doc); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	result, err := collection.InsertOne(context.Background(), doc)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Insertion failed"})
+		return
+	}
+	c.JSON(http.StatusOK, result)
+}
+
+func updateDocument(c *gin.Context) {
+	id := c.Param("id")
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+	var doc map[string]interface{}
+	if err := c.ShouldBindJSON(&doc); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	delete(doc, "_id") // prevent _id from being updated
+	update := bson.M{"$set": doc}
+	_, err = collection.UpdateOne(context.Background(), bson.M{"_id": objID}, update)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Update failed"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "updated"})
+}
+
+func getAllDocuments(c *gin.Context) {
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch"})
+		return
+	}
+	defer cursor.Close(context.Background())
+
+	var documents []Document
+	if err = cursor.All(context.Background(), &documents); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Decoding failed"})
+		return
+	}
+	c.JSON(http.StatusOK, documents)
+}
+
+func getDocumentByID(c *gin.Context) {
+	id := c.Param("id")
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+	var doc Document
+	err = collection.FindOne(context.Background(), bson.M{"_id": objID}).Decode(&doc)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Not found"})
+		return
+	}
+	c.JSON(http.StatusOK, doc)
+}
